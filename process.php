bbd04c307111df52dc2d5372630a7fdad2fa0ffc
Caleb Norris <norrisc129@gmail.com>
2022-04-07 13:44:59 -0400

    Displayed form data, and validated data

--- process.php
+++ process.php
@@ -2,9 +2,36 @@
 
 $name = $_POST['name'];
 
-$cupcakes = $_POST['flavor[]'];
+if (isset($_POST['name'])) {
+    $name= $_POST['name'];
+} else {
+    echo "<br>Error!! Name is required...";
+    return;
+}
 
-echo implode(", ", $cupcakes);
+if (isset($_POST['flavor'])) {
+    $cupcakes = $_POST['flavor'];
+    if (count($cupcakes) < 1) {
+        echo "<br>Error!! At least one cupcake required";
+    }
+} else {
+    echo "<br>Error!! Cupcakes required...";
+    return;
+}
 
-echo $name;
+
+$COST = 3.50;
+$TOTAL = $COST * count($cupcakes);
+if($name)
+echo "<h1>Thank you, $name, for your order!</h1>";
+echo "<p>Order Summary:</p>";
+echo "<ul>";
+foreach($cupcakes as $cake) {
+    echo "<li> $cake </li>";
+}
+echo "</ul>";
+
+
+
+echo "<p>Total: $$TOTAL</p>";
 
